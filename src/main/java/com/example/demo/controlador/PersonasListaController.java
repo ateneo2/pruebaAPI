7145aea7098b94de14bef9515005d904a9822809
ateneo2 <ateneojoseluis@gmail.com>
2023-05-03 13:46:47 -0300

    actualizada PersonasListaController pero datos hardcoded

--- src/main/java/com/example/demo/controlador/PersonasListaController.java
+++ src/main/java/com/example/demo/controlador/PersonasListaController.java
@@ -4,7 +4,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
+//import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.example.demo.modelo.Persona;
